597de2ac3d9ba8383b68053c92243952d44a5d13
caixudong <cxdazxm@163.com>
2020-01-05 22:03:09 +0800

    滑动组件

---
+++ src/components/scroll/index.jsx
@@ -0,0 +1,23 @@
+import React, { forwardRef, useState, useEffect, useRef, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
+import BScorll from 'better-scroll'
+import styled from 'styled-components'
+
+const ScrollContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+`
+
+const Scroll = () => {
+
+
+
+
+    return <ScrollContainer>
+
+    </ScrollContainer>
+}
+
+
+export default Scroll;
